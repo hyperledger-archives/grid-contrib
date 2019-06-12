e53efbe005f43d25f43aa4bf426cbbe70d2bec7e
Darian Plumb <dplumb@bitwise.io>
2019-06-12 15:37:22 -0500

    Fix GetStarted error handling
    Fixes an issue where errors were not getting properly reported in the UI
    
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

--- track_and_trace/asset_client/src/services/auth.js
+++ track_and_trace/asset_client/src/services/auth.js
@@ -1,3 +1,17 @@
+// Copyright 2019 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use strict'
 
 const m = require('mithril')
@@ -234,16 +248,13 @@ const AuthService = {
                     data: userCreate
                 })
                 .catch((e) => {
-                    if (e.error && e.error.status === 400) {
-                        return Promise.reject(e.error.message)
-                    } else {
-                        return Promise.reject('Unable to sign up at this time.')
-                    }
+                    return Promise.reject(JSON.parse(e.message).error)
                 })
                 .then((result) => {
                     if (result.status === 'ok') {
                         api.setAuth(result.authorization)
                         return submitTransactionFn(signer)
+                            .catch((e) => Promise.reject(e))
                     } else {
                         return Promise.reject('Unable to sign up at this time.')
                     }
--- track_and_trace/asset_client/src/views/get_started_form.js
+++ track_and_trace/asset_client/src/views/get_started_form.js
@@ -1,3 +1,17 @@
+// Copyright 2019 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use strict'
 
 const m = require('mithril')
@@ -49,9 +63,9 @@ const GetStarted = {
                 m.route.set('/')
             })
             .catch((e) => {
-                console.error(e)
-                GetStarted.submitting = false
                 GetStarted.error = e
+                GetStarted.submitting = false
+                m.redraw()
             })
     },
 
@@ -72,6 +86,8 @@ const GetStarted = {
             !GetStarted.org_name ||
             !GetStarted.org_address ||
             !GetStarted.email ||
+            !GetStarted.password ||
+            !GetStarted.passwordConfirm ||
             GetStarted.password !== GetStarted.passwordConfirm) {
                 return true
             }
